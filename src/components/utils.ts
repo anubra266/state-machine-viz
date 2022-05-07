dbe641eb528f7bce0ad2ea76c6942924d5db9248
anubra266 <anubra266@gmail.com>
2022-05-07 14:27:21 +0100

    remove rewrites and refactor utils

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,5 +1,5 @@
-import { TypedDocumentNode } from '@graphql-typed-document-node/core';
-import * as React from 'react';
+import { useSelector } from '@xstate/react';
+import React from 'react';
 import {
   ActionObject,
   ActionTypes,
@@ -8,116 +8,7 @@ import {
   Interpreter,
   SendAction,
   StateNode,
-  TransitionDefinition,
 } from 'xstate';
-import {
-  AnyState,
-  AnyStateMachine,
-  EmbedMode,
-  EmbedPanel,
-  ParsedEmbed,
-} from './types';
-import { print } from 'graphql';
-import { useSelector } from '@xstate/react';
-import { NextRouter } from 'next/router';
-
-export function isNullEvent(eventName: string) {
-  return eventName === ActionTypes.NullEvent;
-}
-
-export function isInternalEvent(eventName: string) {
-  const allInternalEventsButNullEvent = Object.values(ActionTypes).filter(
-    (prefix) => !isNullEvent(prefix),
-  );
-  return allInternalEventsButNullEvent.some((prefix) =>
-    eventName.startsWith(prefix),
-  );
-}
-
-export function createInterpreterContext<
-  TInterpreter extends Interpreter<any, any, any>,
->(displayName: string) {
-  const [Provider, useContext] =
-    createRequiredContext<TInterpreter>(displayName);
-
-  const createUseSelector =
-    <Data>(selector: (state: TInterpreter['state']) => Data) =>
-    () => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      return useSelector(useContext(), selector);
-    };
-
-  return [Provider, useContext, createUseSelector] as const;
-}
-
-export function createRequiredContext<T>(displayName: string) {
-  const context = React.createContext<T | null>(null);
-  context.displayName = displayName;
-
-  const useContext = () => {
-    const ctx = React.useContext(context);
-    if (!ctx) {
-      throw new Error(
-        `use${displayName} must be used inside ${displayName}Provider`,
-      );
-    }
-    return ctx;
-  };
-
-  return [context.Provider, useContext] as const;
-}
-
-export interface Edge<
-  TContext,
-  TEvent extends AnyEventObject,
-  TEventType extends TEvent['type'] = string,
-> {
-  event: TEventType;
-  source: StateNode<TContext, any, TEvent>;
-  target: StateNode<TContext, any, TEvent>;
-  transition: TransitionDefinition<TContext, TEvent>;
-  order: number;
-}
-
-export function getChildren(stateNode: StateNode): StateNode[] {
-  if (!stateNode.states) {
-    return [];
-  }
-
-  const children = Object.keys(stateNode.states).map((key) => {
-    return stateNode.states[key];
-  });
-
-  children.sort((a, b) => b.order - a.order);
-
-  return children;
-}
-
-export function getEdges(stateNode: StateNode): Array<Edge<any, any, any>> {
-  const edges: Array<Edge<any, any, any>> = [];
-
-  Object.keys(stateNode.on).forEach((eventType, order) => {
-    const transitions = stateNode.on[eventType];
-
-    transitions.forEach((t) => {
-      const targets = t.target && t.target.length > 0 ? t.target : [stateNode];
-      targets.forEach((target) => {
-        edges.push({
-          event: eventType,
-          source: stateNode,
-          target,
-          transition: t,
-          order,
-        });
-      });
-    });
-  });
-
-  return edges;
-}
-
-export const isStringifiedFunction = (str: string): boolean =>
-  /^function\s*\(/.test(str) || str.includes('=>');
 
 const testPlatform = (re: RegExp): boolean =>
   re.test(globalThis?.navigator?.platform);
@@ -129,55 +20,34 @@ export const isWithPlatformMetaKey = (event: {
   ctrlKey: boolean;
 }) => (isMac() ? event.metaKey : event.ctrlKey);
 
-export const getPlatformMetaKeyLabel = () => (isMac() ? 'CMD' : 'Ctrl');
+const isTextAcceptingInputElement = (input: HTMLInputElement) =>
+  input.type === 'email' ||
+  input.type === 'password' ||
+  input.type === 'search' ||
+  input.type === 'tel' ||
+  input.type === 'text' ||
+  input.type === 'url';
 
-export const updateQueryParamsWithoutReload = (
-  mutator: (queries: URLSearchParams) => void,
-) => {
-  const newURL = new URL(window.location.href);
-  mutator(newURL.searchParams);
-  window.history.pushState({ path: newURL.href }, '', newURL.href);
+const isSvgElement = (el: any): el is SVGElement =>
+  !!el && (/svg/i.test(el.tagName) || !!el.ownerSVGElement);
+
+export const isTextInputLikeElement = (
+  el: HTMLElement | SVGElement,
+): boolean => {
+  return (
+    (el.tagName === 'INPUT' &&
+      isTextAcceptingInputElement(el as HTMLInputElement)) ||
+    el.tagName === 'TEXTAREA' ||
+    (!isSvgElement(el) && el.isContentEditable)
+  );
 };
 
-export const gQuery = <Data, Variables>(
-  query: TypedDocumentNode<Data, Variables>,
-  variables: Variables,
-  accessToken?: string,
-): Promise<{ data?: Data }> =>
-  fetch(process.env.NEXT_PUBLIC_GRAPHQL_API_URL, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      ...(accessToken && { authorization: 'Bearer ' + accessToken }),
-    },
-    body: JSON.stringify({
-      query: print(query),
-      variables,
-    }),
-  })
-    .then((resp) => resp.json())
-    .then((res) => {
-      /**
-       * Throw the GQL error if it comes - this
-       * doesn't happen by default
-       */
-      if (res.errors) {
-        throw new Error(res.errors[0]!.message);
-      }
-      return res;
-    });
+export const isAcceptingArrowKey = (el: HTMLElement | SVGElement): boolean => {
+  return isTextInputLikeElement(el);
+};
 
-export function willChange(
-  machine: AnyStateMachine,
-  state: AnyState,
-  event: AnyEventObject,
-): boolean {
-  return !!machine.transition(state, event).changed;
-}
-
-export function uniq<T>(arr: T[]): T[] {
-  return Array.from(new Set(arr));
-}
+export const isStringifiedFunction = (str: string): boolean =>
+  /^function\s*\(/.test(str) || str.includes('=>');
 
 export function isDelayedTransitionAction(
   action: ActionObject<any, any>,
@@ -200,117 +70,6 @@ export function isDelayedTransitionAction(
       return false;
   }
 }
-
-/**
- * /?mode=viz|full|panels default:viz
- * /?mode=panels&panel=code|state|events|actors default:code
- */
-export const DEFAULT_EMBED_PARAMS: ParsedEmbed = {
-  mode: EmbedMode.Viz,
-  panel: EmbedPanel.Code,
-  showOriginalLink: true,
-  readOnly: true,
-  pan: false,
-  zoom: false,
-  controls: false,
-};
-export const parseEmbedQuery = (query?: NextRouter['query']): ParsedEmbed => {
-  const parsedEmbed = DEFAULT_EMBED_PARAMS;
-
-  const getQueryParamValue = (qParamValue: string | string[]) => {
-    return Array.isArray(qParamValue) ? qParamValue[0] : qParamValue;
-  };
-
-  const computeBooleanQParamValue = (qParamValue: string) => {
-    // Parse to number to treat "0" as false
-    return !!+qParamValue;
-  };
-
-  if (query?.mode) {
-    const parsedMode = getQueryParamValue(query?.mode);
-    if (Object.values(EmbedMode).includes(parsedMode as EmbedMode)) {
-      parsedEmbed.mode = parsedMode as EmbedMode;
-    }
-  }
-
-  if (query?.panel) {
-    const parsedPanel = getQueryParamValue(query?.panel);
-    if (Object.values(EmbedPanel).includes(parsedPanel as EmbedPanel)) {
-      parsedEmbed.panel = parsedPanel as EmbedPanel;
-    }
-  }
-
-  if (query?.showOriginalLink) {
-    const parsedValue = getQueryParamValue(query?.showOriginalLink);
-    parsedEmbed.showOriginalLink = computeBooleanQParamValue(parsedValue);
-  }
-
-  if (query?.readOnly) {
-    const parsedReadOnly = getQueryParamValue(query?.readOnly);
-    parsedEmbed.readOnly = computeBooleanQParamValue(parsedReadOnly);
-  }
-
-  if (query?.pan) {
-    const parsedPan = getQueryParamValue(query?.pan);
-    parsedEmbed.pan = computeBooleanQParamValue(parsedPan);
-  }
-
-  if (query?.zoom) {
-    const parsedZoom = getQueryParamValue(query?.zoom);
-    parsedEmbed.zoom = computeBooleanQParamValue(parsedZoom);
-  }
-
-  if (query?.controls) {
-    const parsedControls = getQueryParamValue(query?.controls);
-    parsedEmbed.controls = computeBooleanQParamValue(parsedControls);
-  }
-
-  return parsedEmbed;
-};
-
-export function calculatePanelIndexByPanelName(panelName: EmbedPanel) {
-  const tabs = Object.values(EmbedPanel);
-  const foundPanelIndex = tabs.findIndex((p) => p === panelName);
-  return foundPanelIndex >= 0 ? foundPanelIndex : 0;
-}
-
-export function withoutEmbedQueryParams(query: any): string {
-  const q = new URLSearchParams(query);
-  // We don't need embed related query params in the original link
-  [
-    'mode',
-    'panel',
-    'showOriginalLink',
-    'pan',
-    'zoom',
-    'controls',
-    'readOnly',
-  ].forEach((key) => {
-    q.delete(key);
-  });
-  return '/viz?' + q.toString();
-}
-const isTextAcceptingInputElement = (input: HTMLInputElement) =>
-  input.type === 'email' ||
-  input.type === 'password' ||
-  input.type === 'search' ||
-  input.type === 'tel' ||
-  input.type === 'text' ||
-  input.type === 'url';
-
-const isSvgElement = (el: any): el is SVGElement =>
-  !!el && (/svg/i.test(el.tagName) || !!el.ownerSVGElement);
-
-export const isTextInputLikeElement = (
-  el: HTMLElement | SVGElement,
-): boolean => {
-  return (
-    (el.tagName === 'INPUT' &&
-      isTextAcceptingInputElement(el as HTMLInputElement)) ||
-    el.tagName === 'TEXTAREA' ||
-    (!isSvgElement(el) && el.isContentEditable)
-  );
-};
 
 const getRoles = (el: HTMLElement | SVGElement): string[] => {
   const explicitRole = el.getAttribute('role');
@@ -364,62 +123,21 @@ const getRoles = (el: HTMLElement | SVGElement): string[] => {
   }
 };
 
-export const isAcceptingArrowKey = (el: HTMLElement | SVGElement): boolean => {
-  if (isTextInputLikeElement(el)) {
-    return true;
-  }
+export function createRequiredContext<T>(displayName: string) {
+  const context = React.createContext<T | null>(null);
+  context.displayName = displayName;
 
-  if (el.tagName === 'INPUT') {
-    const input = el as HTMLInputElement;
-    return input.type === 'range';
-  }
-
-  // TODO: support accordion headers, aria grids, treegrid (keep in midn that treegrid can contain interactive elements)
-  const rolesWithArrowsSupport: Record<string, boolean> = {
-    // not every button accepts arrows but let's assume that they all do since menu buttons and buttons in toolbars accept them
-    // but there is no good way to quickly differentiate those
-    button: true,
-    combobox: true,
-    listbox: true,
-    menu: true,
-    menubar: true,
-    menuitem: true,
-    radio: true,
-    radiogroup: true,
-    tab: true,
-    textbox: true,
-    treeitem: true,
-    tree: true,
+  const useContext = () => {
+    const ctx = React.useContext(context);
+    if (!ctx) {
+      throw new Error(
+        `use${displayName} must be used inside ${displayName}Provider`,
+      );
+    }
+    return ctx;
   };
-  return getRoles(el).some((role) => rolesWithArrowsSupport[role]);
-};
 
-export function paramsToRecord(
-  params: { name: string; value: string | boolean }[],
-): ParsedEmbed {
-  return params.reduce(
-    (result, current) => ({
-      ...result,
-      [current.name]: current.value,
-    }),
-    {} as ParsedEmbed,
-  );
-}
-
-export function makeEmbedUrl(id: string, baseUrl: string, params: ParsedEmbed) {
-  const paramsWithNumberValues = Object.entries(params).reduce(
-    (result, current) => {
-      return {
-        ...result,
-        // Convert true|false to 1|0
-        [current[0]]:
-          typeof current[1] === 'boolean' ? +current[1] : current[1],
-      };
-    },
-    {},
-  );
-  const query = new URLSearchParams(paramsWithNumberValues as any);
-  return `${baseUrl}/viz/embed/${id}?${query.toString()}`;
+  return [context.Provider, useContext] as const;
 }
 
 export const isAcceptingSpaceNatively = (
@@ -429,3 +147,33 @@ export const isAcceptingSpaceNatively = (
   el.tagName === 'INPUT' ||
   isTextInputLikeElement(el) ||
   getRoles(el).includes('button');
+
+export function getChildren(stateNode: StateNode): StateNode[] {
+  if (!stateNode.states) {
+    return [];
+  }
+
+  const children = Object.keys(stateNode.states).map((key) => {
+    return stateNode.states[key];
+  });
+
+  children.sort((a, b) => b.order - a.order);
+
+  return children;
+}
+
+export function createInterpreterContext<
+  TInterpreter extends Interpreter<any, any, any>,
+>(displayName: string) {
+  const [Provider, useContext] =
+    createRequiredContext<TInterpreter>(displayName);
+
+  const createUseSelector =
+    <Data>(selector: (state: TInterpreter['state']) => Data) =>
+    () => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      return useSelector(useContext(), selector);
+    };
+
+  return [Provider, useContext, createUseSelector] as const;
+}
