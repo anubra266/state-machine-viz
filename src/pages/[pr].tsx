17d0c84347218946b9884513f5cad3bb11520cb8
anubra266 <anubra266@gmail.com>
2022-05-08 11:16:39 +0100

    feat: 404 message for non existent pull requests

--- src/pages/[pr].tsx
+++ src/pages/[pr].tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { MachineVisualizer } from '../components/MachineVisualizer';
-import { actions } from 'xstate';
+import { actions, createMachine } from 'xstate';
 import { parseMachine } from '../components/parseMachine';
 import { GetServerSideProps } from 'next';
 const { Octokit } = require('@octokit/core');
 
-const { choose } = actions;
+function App(props: { machineSource: string; status: Record<string, any> }) {
+  let machine;
 
-function App(props: { machineSource: string }) {
-  const machine = parseMachine(props.machineSource);
+  if (!props.status.successful) {
+    machine = createMachine(props.status.message);
+  } else machine = parseMachine(props.machineSource);
 
   return <MachineVisualizer machine={machine} />;
 }
@@ -19,25 +21,45 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const prNumber = context.params?.pr;
   const MACHINE_OUTPUT_FILE = 'output.js';
   const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
-  const response = await octokit.request(
-    'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
-    {
-      owner: 'chakra-ui',
-      repo: 'zag',
-      pull_number: prNumber,
-    },
-  );
 
-  const prFiles = response.data as Record<string, any>[];
+  async function getData() {
+    try {
+      const response = await octokit.request(
+        'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
+        {
+          owner: 'chakra-ui',
+          repo: 'zag',
+          pull_number: prNumber,
+        },
+      );
+      const prFiles = response.data as Record<string, any>[];
 
-  const outputFile = prFiles.find(
-    (file) => file.filename === MACHINE_OUTPUT_FILE,
-  );
+      const outputFile = prFiles.find(
+        (file) => file.filename === MACHINE_OUTPUT_FILE,
+      );
 
-  const fileContentURI = outputFile?.raw_url;
-  const fileContent = await fetch(fileContentURI).then((r) => r.text());
+      const fileContentURI = outputFile?.raw_url;
+      const fileContent = await fetch(fileContentURI).then((r) => r.text());
+      return fileContent;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  const fileContent = await getData();
+  let status;
+  if (fileContent) {
+    status = {
+      successful: true,
+    };
+  } else {
+    status = {
+      successful: false,
+      message: { id: `Pull request ${prNumber} not found` },
+    };
+  }
 
   return {
-    props: { machineSource: fileContent },
+    props: { machineSource: fileContent, status },
   };
 };
